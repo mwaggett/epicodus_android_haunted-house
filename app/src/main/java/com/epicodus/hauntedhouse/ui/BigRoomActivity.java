8c16fb7b1eea514424f0b87fdfab990b22bf574b
Molly Waggett <mwaggett@alumni.reed.edu>
2015-10-29 10:50:29 -0700

    Add method for toggling button visibility in BigRoomActivity and attach onClick listeners to Hallway and Staircase buttons.

--- app/src/main/java/com/epicodus/hauntedhouse/ui/BigRoomActivity.java
+++ app/src/main/java/com/epicodus/hauntedhouse/ui/BigRoomActivity.java
@@ -1,19 +1,16 @@
 package com.epicodus.hauntedhouse.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.epicodus.hauntedhouse.R;
-import com.epicodus.hauntedhouse.models.Item;
 import com.epicodus.hauntedhouse.models.Player;
 
 public class BigRoomActivity extends AppCompatActivity {
@@ -25,6 +22,9 @@ public class BigRoomActivity extends AppCompatActivity {
     private ImageView mRoomImage;
     private Button mYesButton;
     private Button mNoButton;
+    private Button mHallwayButton;
+    private Button mStairsButton;
+    private Button mExploreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,36 +41,62 @@ public class BigRoomActivity extends AppCompatActivity {
         mRoomImage = (ImageView) findViewById(R.id.roomImage);
         mYesButton = (Button) findViewById(R.id.yesButton);
         mNoButton = (Button) findViewById(R.id.noButton);
+        mHallwayButton = (Button) findViewById(R.id.hallwayButton);
+        mStairsButton = (Button) findViewById(R.id.roomToStairsButton);
+        mExploreButton = (Button) findViewById(R.id.exploreRoomButton);
 
         if (mPlayer.checkInventory("flashlight")) {
             mRoomText.setText("It is dark. Do you turn on your flashlight?");
-            mYesButton.setVisibility(View.VISIBLE);
-            mNoButton.setVisibility(View.VISIBLE);
+            toggleButtonVisibility();
 
             mYesButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mRoomImage.setImageResource(R.drawable.big_room);
                     mRoomText.setText("There is a dusty old couch, an antique piano, and sheet music scattering the floor.");
-                    mYesButton.setVisibility(View.INVISIBLE);
-                    mNoButton.setVisibility(View.INVISIBLE);
+                    toggleButtonVisibility();
                 }
             });
             mNoButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mRoomText.setText("You cannot see.");
-                    mYesButton.setVisibility(View.INVISIBLE);
-                    mNoButton.setVisibility(View.INVISIBLE);
+                    toggleButtonVisibility();
                 }
             });
         }
 
+        mHallwayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BigRoomActivity.this, HallwayActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mStairsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BigRoomActivity.this, StaircaseActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-//    private void displayRoomChoice() {
-//        mRoomText.setText("something else");
-//        mYesButton.setVisibility(View.INVISIBLE);
-//        mNoButton.setVisibility(View.INVISIBLE);
-//    }
+    private void toggleButtonVisibility() {
+        if (mYesButton.getVisibility()==View.VISIBLE) {
+            mYesButton.setVisibility(View.INVISIBLE);
+            mNoButton.setVisibility(View.INVISIBLE);
+            mHallwayButton.setVisibility(View.VISIBLE);
+            mStairsButton.setVisibility(View.VISIBLE);
+            mExploreButton.setVisibility(View.VISIBLE);
+        } else {
+            mYesButton.setVisibility(View.VISIBLE);
+            mNoButton.setVisibility(View.VISIBLE);
+            mHallwayButton.setVisibility(View.INVISIBLE);
+            mStairsButton.setVisibility(View.INVISIBLE);
+            mExploreButton.setVisibility(View.INVISIBLE);
+        }
+    }
 }
--- app/src/main/java/com/epicodus/hauntedhouse/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/hauntedhouse/ui/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
